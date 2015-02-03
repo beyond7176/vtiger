8da6db305522d7095023a8efdb64d6fe2acec79f
chenhs <cyj91516@aliyun.com>
2015-02-03 16:18:40 +0800

    commit

--- user_privileges/user_privileges_7.php
+++ user_privileges/user_privileges_7.php
@@ -12,17 +12,17 @@ $current_user_profiles=array(5,);
 
 $profileGlobalPermission=array('1'=>1,'2'=>1,);
 
-$profileTabsPermission=array('1'=>0,'2'=>0,'4'=>0,'6'=>0,'7'=>0,'8'=>0,'9'=>0,'10'=>0,'13'=>0,'14'=>0,'15'=>0,'16'=>0,'18'=>0,'19'=>0,'20'=>0,'21'=>0,'22'=>0,'23'=>0,'24'=>0,'25'=>0,'26'=>0,'31'=>0,'34'=>0,'35'=>0,'37'=>0,'39'=>0,'40'=>0,'41'=>0,'42'=>0,'43'=>0,'44'=>0,'45'=>0,'46'=>0,'47'=>0,'28'=>0,'3'=>0,);
+$profileTabsPermission=array('1'=>0,'2'=>0,'4'=>0,'6'=>0,'7'=>0,'8'=>0,'9'=>0,'10'=>0,'13'=>0,'14'=>0,'15'=>0,'16'=>0,'18'=>0,'19'=>0,'20'=>0,'21'=>0,'22'=>0,'23'=>0,'24'=>0,'25'=>0,'26'=>0,'31'=>0,'34'=>0,'35'=>0,'37'=>0,'39'=>0,'40'=>0,'41'=>0,'42'=>0,'43'=>0,'44'=>0,'45'=>0,'46'=>0,'47'=>0,'48'=>0,'28'=>0,'3'=>0,);
 
 $profileActionPermission=array(2=>array(0=>1,1=>1,2=>1,4=>0,10=>0,),4=>array(0=>0,1=>0,2=>0,4=>0,10=>0,),6=>array(0=>0,1=>0,2=>0,4=>0,10=>0,),7=>array(0=>1,1=>1,2=>1,4=>0,9=>0,10=>0,),8=>array(0=>0,1=>0,2=>0,4=>0,),9=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,),13=>array(0=>0,1=>0,2=>0,4=>0,10=>0,),14=>array(0=>0,1=>0,2=>0,4=>0,10=>0,),15=>array(0=>0,1=>0,2=>0,4=>0,),16=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,),18=>array(0=>0,1=>0,2=>0,4=>0,10=>0,),19=>array(0=>1,1=>1,2=>1,4=>0,),20=>array(0=>1,1=>1,2=>1,4=>0,5=>0,6=>0,),21=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,),22=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,),23=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,),25=>array(0=>1,1=>1,2=>1,4=>1,6=>0,11=>0,),26=>array(0=>1,1=>1,2=>1,4=>0,),34=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,10=>0,),35=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,10=>0,),37=>array(0=>0,1=>0,2=>0,4=>0,),41=>array(0=>0,1=>0,2=>0,4=>0,),42=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,10=>0,),43=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,10=>0,),44=>array(0=>0,1=>0,2=>0,4=>0,5=>0,6=>0,10=>0,),46=>array(0=>0,1=>0,2=>0,4=>0,),);
 
 $current_user_groups=array();
 
-$subordinate_roles=array();
+$subordinate_roles=array('H7',);
 
 $parent_roles=array('H1',);
 
-$subordinate_roles_users=array();
+$subordinate_roles_users=array('H7'=>array(),);
 
 $user_info=array('user_name'=>'test','is_admin'=>'off','user_password'=>'$1$te000000$NxMCb7p8Ey2zEPIQohrFO/','confirm_password'=>'$1$te000000$NxMCb7p8Ey2zEPIQohrFO/','first_name'=>'','last_name'=>'test','roleid'=>'H2','email1'=>'abc@d.e','status'=>'Active','activity_view'=>'Today','lead_view'=>'Today','hour_format'=>'12','end_hour'=>'','start_hour'=>'00:00','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'yyyy-mm-dd','signature'=>'','description'=>'','address_street'=>'','address_city'=>'','address_state'=>'','address_postalcode'=>'','address_country'=>'','accesskey'=>'WizelY6YeyHAIO2F','time_zone'=>'Asia/Brunei','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'.','currency_grouping_separator'=>',','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'0','theme'=>'softed','language'=>'zh_cn','reminder_interval'=>'','no_of_currency_decimals'=>'2','truncate_trailing_zeros'=>'0','dayoftheweek'=>'Monday','callduration'=>'5','othereventduration'=>'5','calendarsharedtype'=>'public','default_record_view'=>'Summary','leftpanelhide'=>'1','rowheight'=>'medium','defaulteventstatus'=>'请选择','defaultactivitytype'=>'请选择','hidecompletedevents'=>'0','is_owner'=>'0','currency_name'=>'China, Yuan Renminbi','currency_code'=>'CNY','currency_symbol'=>'&#165;','conv_rate'=>'1.00000','record_id'=>'','record_module'=>'','id'=>'7');
 ?>
